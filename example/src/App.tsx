a68939177810290a01e41becce2021344b06b388
Lee Hoe Mun <leehoemun@gmail.com>
2021-05-30 18:33:24 +0800

    chore: first commit

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,82 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
 import AwesomeCarousel from 'react-native-awesome-carousel';
 
+const { width } = Dimensions.get('window');
+const height = 100;
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    AwesomeCarousel.multiply(3, 7).then(setResult);
-  }, []);
-
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar translucent={false} />
+      <Text style={styles.title}>Normal Carousel</Text>
+      <View style={styles.slideContainer}>
+        <AwesomeCarousel loop={false}>
+          <View style={styles.slide1}>
+            <Text>Normal Carousel</Text>
+            <Text>Slide 1</Text>
+          </View>
+          <View style={styles.slide2}>
+            <Text>Normal Carousel</Text>
+            <Text>Slide 2</Text>
+          </View>
+        </AwesomeCarousel>
+      </View>
+
+      <Text style={styles.title}>Autoplay loop Carousel</Text>
+      <View style={styles.slideContainer}>
+        <AwesomeCarousel autoplay={true} loop={true}>
+          <View style={styles.slide1}>
+            <Text>Normal Carousel</Text>
+            <Text>Slide 1</Text>
+          </View>
+          <View style={styles.slide2}>
+            <Text>Normal Carousel</Text>
+            <Text>Slide 2</Text>
+          </View>
+        </AwesomeCarousel>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   box: {
     width: 60,
     height: 60,
     marginVertical: 20,
+  },
+  slideContainer: {
+    marginBottom: 10,
+    height,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 6,
+  },
+  slide1: {
+    height,
+    width,
+    backgroundColor: '#FED7AA',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  slide2: {
+    height,
+    width,
+    backgroundColor: '#FECACA',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,94 @@
-export default {
-  multiply(a: number, b: number) {
-    return Promise.resolve(a * b);
-  },
+import React, { useEffect, useRef, useState } from 'react';
+import PagerView, {
+  PagerViewOnPageSelectedEvent,
+  PageScrollStateChangedNativeEvent,
+} from 'react-native-pager-view';
+
+interface IAwesomeCarousel {
+  children: JSX.Element[];
+  loop?: boolean;
+  autoplay?: boolean;
+}
+
+const AwesomeCarousel = ({
+  children = [],
+  loop = true,
+  autoplay = true,
+}: IAwesomeCarousel) => {
+  const viewPagerRef = useRef<PagerView>(null);
+  const [components, setComponents] = useState<JSX.Element[]>([]);
+  const [scrollingState, setScrollingState] = useState('idle');
+  const [currentPage, setCurrentPage] = useState(1);
+  const timeoutRef = useRef<NodeJS.Timeout[]>([]);
+
+  useEffect(() => {
+    if (scrollingState === 'idle' && loop) {
+      const reachedFakeLastSlide = currentPage === 0;
+      const reachedFakeFirstSlide = currentPage === components.length - 1;
+
+      if (reachedFakeFirstSlide) {
+        viewPagerRef?.current?.setPageWithoutAnimation(1);
+      } else if (reachedFakeLastSlide) {
+        viewPagerRef?.current?.setPageWithoutAnimation(components.length - 2);
+      }
+    }
+  }, [scrollingState, components.length, currentPage, loop]);
+
+  useEffect(() => {
+    if (
+      components.length > 1 &&
+      currentPage !== 0 &&
+      currentPage !== components.length - 1 &&
+      autoplay
+    ) {
+      const currentTimeout = setTimeout(() => {
+        if (viewPagerRef?.current) {
+          viewPagerRef.current.setPage(currentPage + 1);
+        }
+      }, 2500);
+
+      timeoutRef.current.push(currentTimeout);
+    }
+  }, [viewPagerRef, currentPage, components, autoplay]);
+
+  useEffect(() => {
+    if (children.length > 1 && loop) {
+      const transformchildren = [
+        children[children.length - 1],
+        ...children,
+        children[0],
+      ];
+      setComponents(transformchildren);
+    } else {
+      setCurrentPage(0);
+      setComponents(children);
+    }
+  }, [loop, children]);
+
+  const _onPageSelected = (event: PagerViewOnPageSelectedEvent) => {
+    setCurrentPage(event.nativeEvent.position);
+  };
+
+  const _onPageScrollStateChanged = (e: PageScrollStateChangedNativeEvent) => {
+    if (e.nativeEvent.pageScrollState === 'dragging') {
+      timeoutRef.current.map((v) => {
+        clearTimeout(v);
+      });
+    }
+    setScrollingState(e.nativeEvent.pageScrollState);
+  };
+
+  return (
+    <PagerView
+      ref={viewPagerRef}
+      style={{ flex: 1 }}
+      initialPage={currentPage}
+      onPageScrollStateChanged={_onPageScrollStateChanged}
+      onPageSelected={_onPageSelected}
+    >
+      {components}
+    </PagerView>
+  );
 };
+
+export default AwesomeCarousel;
